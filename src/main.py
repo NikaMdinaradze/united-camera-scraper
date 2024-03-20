bf48427556125e6509bf6fce99ec467db0a4b574
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-03-20 14:24:16 +0400

    Chore: move driver configuration to settings

--- src/main.py
+++ src/main.py
@@ -1,26 +1,10 @@
-from fake_useragent import UserAgent
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
 from cameras import CameraManager
 from extractors.canon import CanonExtractor
 from extractors.nikon import NikonExtractor
 from extractors.sony import SonyExtractor
+from settings import get_driver
 
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--incognito")
-
-# Generating random user-agents
-ua = UserAgent()
-user_agent = ua.random
-
-chrome_options.add_argument(f"--user-agent={user_agent}")
-
-driver = webdriver.Chrome(options=chrome_options)
-driver.maximize_window()
+driver = get_driver()
 
 nikon_extractor = NikonExtractor()
 CameraManager.execute_extractor(nikon_extractor, driver)
--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,7 @@
 import os
+from fake_useragent import UserAgent
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from pymongo import MongoClient
 
@@ -15,3 +18,22 @@ collection = db["cameras"]
 
 # chatgpt configuration
 API_KEY = os.getenv("API_KEY")
+
+# driver configuration
+def get_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--incognito")
+
+    # Generating random user-agents
+    ua = UserAgent()
+    user_agent = ua.random
+
+    chrome_options.add_argument(f"--user-agent={user_agent}")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.maximize_window()
+
+    return driver
